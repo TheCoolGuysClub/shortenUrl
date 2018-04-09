d50cb5ee41b5713b00615f2303ddcccd1689e064
DavidWei95036593 <dw36593@pausd.us>
2018-04-09 13:20:02 -0700

    add gitignore

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/shortened_url');
+mongoose.connect(process.env.MONGODB_URL||'mongodb://localhost:27017/shortened_url');
 
 
 module.exports = {mongoose}
